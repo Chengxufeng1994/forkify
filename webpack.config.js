15377660669e06f1e0905d424426a005d5dbf7e8
Xu Feng Cheng <benny12628@gmail.com>
2020-07-26 20:22:56 +0800

    A Modern Setup: Babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/js/index.js'),
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src/index.jsx')],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   plugins: [
     // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.html',
+      template: './public/index.html',
     }),
   ],
   devServer: {
